abe9ac18f878e2c319567e8826c39e07e4584e3c
Navneet Prakash <navprak@gmail.com>
2017-01-19 10:45:43 +1300

    Initial Commit

---
+++ Facebook Style Photos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook Style Photos
+//
+//  Created by Navneet Prakash on 19/01/17.
+//  Copyright © 2017 Navneet Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
