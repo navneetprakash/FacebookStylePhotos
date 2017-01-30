031f0970147a8a3a55ed6548fb9be25123471db5
Navneet Prakash <navprak@gmail.com>
2017-01-31 09:50:14 +1300

    upload

---
+++ Facebook Style Photos/ImagesCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesCell.swift
+//  Facebook Style Photos
+//
+//  Created by Navneet Prakash on 19/01/17.
+//  Copyright © 2017 Navneet Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ImagesCell: UICollectionViewCell {
+    
+}
